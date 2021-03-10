c9486b3d9147d8e899775b7b647c67e3991a5b13
729693 <vinaymandava25@gmail.com>
2021-03-10 23:45:28 +0530

    First commit

---
+++ src/main/java/com/vinay/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.vinay.springjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/vinay/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
